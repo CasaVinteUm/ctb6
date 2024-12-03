f9f3daca342ef3fc4962f63214876740e39f9ebc
jaonoctus <jaonoctus@protonmail.com>
2024-12-02 21:10:43 -0300

    soul: alice

---
+++ souls/alice/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CTB6.Souls.Alice;
+
+using NBitcoin;
+
+internal class Program
+{
+    private static readonly string _xpriv =
+        "xprv9s21ZrQH143K3kVxaer4FSKkM5W398t9VmMGLxmedqb8Ndw9N6rfcTHAfUyAYJ9o12TMQX7jd2Z6xGpuWBeBz4SfbCJP7tdvVsPkizKBtG2";
+    private static readonly KeyPath _derivationPath = new("m/86'/0'/0'/0/0");
+    private static readonly string _targetAddress = "bc1pw8d5j3vecstsc9kpahpdfy4xfuct6xn7cqeaxsk6w2ylz63s850q7sragu";
+    private static readonly Network _network = Network.Main;
+    private static readonly ScriptPubKeyType _type = ScriptPubKeyType.TaprootBIP86;
+
+    public static void Main(string[] args)
+    {
+        var node = ExtKey.Parse(_xpriv, _network);
+        var childNode = node.Derive(_derivationPath);
+        var computedAddress = childNode.PrivateKey.GetAddress(_type, _network).ToString();
+
+        if (!computedAddress.Equals(_targetAddress))
+        {
+            throw new ApplicationException("Address mismatch");
+        }
+
+        var publicKey = childNode.PrivateKey.PubKey;
+        var privateKeyWIF = childNode.PrivateKey.GetWif(_network);
+
+        Console.WriteLine("### Alice");
+        Console.WriteLine($"Derivation Path:\t{_derivationPath}");
+        Console.WriteLine($"Address:\t\t{computedAddress}");
+        Console.WriteLine($"Public Key:\t\t{publicKey}");
+        Console.WriteLine($"Private Key:\t\t{privateKeyWIF}");
+    }
+}
---
+++ souls/alice/README.md
@@ -0,0 +1,14 @@
+# Apathetic Alice
+
+> "The other souls call me Apathetic Alice because `I never took the time to understand Xprivs`. 
+My wallet exported this for me but I don't have the slightest clue on what it is or what to do with it. 
+I'm used to seeing a 12 or 24 word seed. I think this somehow holding my UXTO that is trapping me. Please help.”
+
+## Solution
+
+```
+Derivation Path:        86'/0'/0'/0/0
+Address:                bc1pw8d5j3vecstsc9kpahpdfy4xfuct6xn7cqeaxsk6w2ylz63s850q7sragu
+Public Key:             023e66ec249a1332b90b33b6f352bdbec339f17e0b3d9aec8db5f5ad068df286d1
+Private Key:            KzDNFCJaLrUU65A6QQvApxe5GxSScWKCSa58LVMeLhw4N3T6csuP
+```
