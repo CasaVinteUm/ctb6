bfa994b8c62226b639ff35cb014f49bf217775a8
jaonoctus <jaonoctus@protonmail.com>
2024-12-10 00:53:31 -0300

    soul: dave

---
+++ souls/dave/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CTB6.Souls.Dave;
+
+using NBitcoin;
+
+internal class Program
+{
+    private static readonly string _xpriv =
+        "xprv9s21ZrQH143K4PBx8nb1j4BEpSt7cbBdjzqqZbYcbRREfCxzAqdnxrDRD9kQv8VudFo1BavMT8s7Lq8HH8VwSZAxyGrFoHTC5fF2LZMs8J4";
+    private static readonly KeyPath _derivationPath = new("m/69'/420'/999999999'/8008135'/0");
+    private static readonly string _targetAddress = "bc1pjfpqgtuztfcwne84xxe294xnamphu7thj5gv3v2mp8j3rgaqd2gsnydssw";
+    private static readonly Network _network = Network.Main;
+    private static readonly ScriptPubKeyType _type = ScriptPubKeyType.TaprootBIP86;
+
+    public static void Main(string[] args)
+    {
+        var node = ExtKey.Parse(_xpriv, _network);
+        var childNode = node.Derive(_derivationPath);
+        var computedAddress = childNode.PrivateKey.GetAddress(_type, _network).ToString();
+
+        if (!computedAddress.Equals(_targetAddress))
+        {
+            throw new ApplicationException("Address mismatch");
+        }
+
+        var publicKey = childNode.PrivateKey.PubKey;
+        var privateKeyWIF = childNode.PrivateKey.GetWif(_network);
+
+        Console.WriteLine("### Dave");
+        Console.WriteLine($"Derivation Path:\t{_derivationPath}");
+        Console.WriteLine($"Address:\t\t{computedAddress}");
+        Console.WriteLine($"Public Key:\t\t{publicKey}");
+        Console.WriteLine($"Private Key:\t\t{privateKeyWIF}");
+    }
+}
---
+++ souls/dave/README.md
@@ -0,0 +1,23 @@
+# Derivation Dave
+
+> "You thought Charlie was being clever? Oh boy, `I used some crazy derivation path` to secure my funds, 
+> and now I'm at a total loss as to how my UTXO will ever be released. 
+> The good news is, I still have access to my Xpriv, but my derivation path has vanished into thin air, 
+> and this evil taproot tree won’t reveal it! 
+> It couldn’t have gone far—`check around open areas` to see if you can find any hints. As spooky as this place is, 
+> I heard there’s a tech conference going on, so make sure not to interrupt any workshops or talks. 
+> I’m pretty sure the clues should all be in accessible areas. Once you find the derivation path, 
+> you can also use the same derivation path with my Xpriv to defeat the taproot tree's dark script!"
+
+```
+xprv9s21ZrQH143K4PBx8nb1j4BEpSt7cbBdjzqqZbYcbRREfCxzAqdnxrDRD9kQv8VudFo1BavMT8s7Lq8HH8VwSZAxyGrFoHTC5fF2LZMs8J4
+```
+
+## Solution
+
+```
+Derivation Path:        69'/420'/999999999'/8008135'/0
+Address:                bc1pjfpqgtuztfcwne84xxe294xnamphu7thj5gv3v2mp8j3rgaqd2gsnydssw
+Public Key:             0383835dbe036944f18783e0a525babe23965a2b4fdeca2d2d84997fc6ff0fb06a
+Private Key:            L2u5zbZXbMMuZZ3buvkccJzPb4WFhuqaBggXNief63cjUq2foFcu
+```
