d1643fa6d62d574d39df8262b1972c2718c91a67
jaonoctus <jaonoctus@protonmail.com>
2024-12-26 14:00:18 -0300

    soul: judy

---
+++ souls/judy/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CTB6.Souls.Judy;
+
+using NBitcoin;
+
+internal class Program
+{
+    private static readonly Mnemonic _mnemonic = new("find all seed word they work when combine then become lucky tonight");
+    private static readonly string _passphrase = "tabconfisgoat";
+    private static readonly KeyPath _derivationPath = new("m/86'/0'/0'/0/0");
+    private static readonly string _targetAddress = "bc1pnre2d0g2caks4745xa6q2cqh9j39z0s0fgk7w9crvwtk28lskm5qxwy4d9";
+    private static readonly Network _network = Network.Main;
+    private static readonly ScriptPubKeyType _type = ScriptPubKeyType.TaprootBIP86;
+
+    public static void Main(string[] args)
+    {
+        var node = _mnemonic.DeriveExtKey(_passphrase);
+        var childNode = node.Derive(_derivationPath);
+        var computedAddress = childNode.PrivateKey.GetAddress(_type, _network).ToString();
+
+        if (!computedAddress.Equals(_targetAddress))
+        {
+            throw new ApplicationException("Address mismatch");
+        }
+
+        var publicKey = childNode.PrivateKey.PubKey;
+        var privateKeyWIF = childNode.PrivateKey.GetWif(_network);
+
+        Console.WriteLine("### Judy");
+        Console.WriteLine($"Derivation Path:\t{_derivationPath}");
+        Console.WriteLine($"Address:\t\t{computedAddress}");
+        Console.WriteLine($"Mnemonic:\t\t{_mnemonic}");
+        Console.WriteLine($"Passphrase:\t\t{_passphrase}");
+        Console.WriteLine($"Public Key:\t\t{publicKey}");
+        Console.WriteLine($"Private Key:\t\t{privateKeyWIF}");
+    }
+}
---
+++ souls/judy/README.md
@@ -0,0 +1,20 @@
+# Jumbled Judy
+
+> "This tree is pure evil! It took my seed phrase, jumbled it up, and somehow automagically sent it to the nearby tech conference. 
+> The tree totally horcrux'd me into 12 pieces, `and now every attendee has a part of the information`. 
+> Try working with others to figure it out. I'm counting on you!"
+
+```
+86'/0'/0'/0/*
+```
+
+## Solution
+
+```
+Derivation Path:        86'/0'/0'/0/0
+Address:                bc1pnre2d0g2caks4745xa6q2cqh9j39z0s0fgk7w9crvwtk28lskm5qxwy4d9
+Mnemonic:               find all seed word they work when combine then become lucky tonight
+Passphrase:             tabconfisgoat
+Public Key:             02ad0c40157f34da06b87c3765bfd3e84a6ba994b4933fdddf0eb4aed49abf45be
+Private Key:            KyXSRcD2UUNo8gPnaCSVSAWcUKveZDgHAtjvXR2MeZSdAMxnmizg
+```
