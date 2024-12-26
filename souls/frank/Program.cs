0d828976a5165c8572c2beb7bd43df942f083ea6
jaonoctus <jaonoctus@protonmail.com>
2024-12-26 13:52:28 -0300

    soul: frank

---
+++ souls/frank/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CTB6.Souls.Frank;
+
+using NBitcoin;
+
+internal class Program
+{
+    private static readonly Mnemonic _mnemonic = new("please remember forget nothing need find all seed word they work okay");
+    private static readonly KeyPath _derivationPath = new("m/86'/0'/0'/0/0");
+    private static readonly string _targetAddress = "bc1pgz8gkuacz8s5nwvrycm6sslx5unu7pqmzyg24gujwnhp993wvx9sjv6xtu";
+    private static readonly Network _network = Network.Main;
+    private static readonly ScriptPubKeyType _type = ScriptPubKeyType.TaprootBIP86;
+
+    public static void Main(string[] args)
+    {
+        var node = _mnemonic.DeriveExtKey();
+        var childNode = node.Derive(_derivationPath);
+        var computedAddress = childNode.PrivateKey.GetAddress(_type, _network).ToString();
+
+        if (!computedAddress.Equals(_targetAddress))
+        {
+            throw new ApplicationException("Address mismatch");
+        }
+
+        var publicKey = childNode.PrivateKey.PubKey;
+        var privateKeyWIF = childNode.PrivateKey.GetWif(_network);
+
+        Console.WriteLine("### Frank");
+        Console.WriteLine($"Derivation Path:\t{_derivationPath}");
+        Console.WriteLine($"Address:\t\t{computedAddress}");
+        Console.WriteLine($"Mnemonic:\t\t{_mnemonic}");
+        Console.WriteLine($"Public Key:\t\t{publicKey}");
+        Console.WriteLine($"Private Key:\t\t{privateKeyWIF}");
+    }
+}
---
+++ souls/frank/README.md
@@ -0,0 +1,21 @@
+# Forgetful Frank
+
+> "Well, I'm the newest here. I thought I'd be helpful and volunteer at TABConf, but didn't expect to meet my fate here. 
+> So, `I wanted to set some ground rules for the conference`, but, uh, things didn’t exactly go as planned obviously, 
+> I heard voices and found this tree, and before you know it I made a deal with Grim and the tree captured me in a UTXO. 
+> The good news is I left some rules around the conference that can free my soul, but I'm forgetting what my rules were. 
+> I’m pretty sure the clues should all be in accessible areas."
+
+```
+86'/0'/0'/0/*
+```
+
+## Solution
+
+```
+Derivation Path:        86'/0'/0'/0/0
+Address:                bc1pgz8gkuacz8s5nwvrycm6sslx5unu7pqmzyg24gujwnhp993wvx9sjv6xtu
+Mnemonic:               please remember forget nothing need find all seed word they work okay
+Public Key:             02a7060f90e50ccebe2b0fef15631760310ec01b0bcc001f4df2bb3dceda08b373
+Private Key:            Kwj92dXyGTZDuqhuVM1v6Xkbi55AQjFq7Z5nTX6DPB4SaWJaoNjn
+```
